993d3fde7d1cc139c099bf8c5713bdff74a9ad71
tab0902 <tabuchi@ewewing.com>
2019-11-02 13:15:39 +0900

    Remove semi-colons in main.js

--- bookplaylist/static/js/main.js
+++ bookplaylist/static/js/main.js
@@ -1,31 +1,31 @@
 $(function() {
 
   // drawer menu
-  var height;
-  var scrollpos;
-  var header = $('#header').height();
+  var height
+  var scrollpos
+  var header = $('#header').height()
 
-  $('#dummy').css('height', 99999);
-  $('#drawer').css('padding-top', header);
-  $('#content').css('padding-top', header);
+  $('#dummy').css('height', 99999)
+  $('#drawer').css('padding-top', header)
+  $('#content').css('padding-top', header)
 
   $('#hamburger').on('click', function() {
-    $(this).toggleClass('active');
+    $(this).toggleClass('active')
     if ($(this).hasClass('active')) {
-      scrollpos = $(window).scrollTop();
-      height = scrollpos - header;
-      $('#dummy').fadeIn();
-      $('#drawer').animate({height: 'toggle'});
-      $('#content').addClass('fixed').css('top', -scrollpos);
+      scrollpos = $(window).scrollTop()
+      height = scrollpos - header
+      $('#dummy').fadeIn()
+      $('#drawer').animate({height: 'toggle'})
+      $('#content').addClass('fixed').css('top', -scrollpos)
     } else {
-      $('#dummy').fadeOut();
+      $('#dummy').fadeOut()
       $('#drawer').animate({height: 'toggle'}, function() {
-        $('#content').removeClass('fixed').css('top', height);
-        $('body,html').animate({scrollTop: scrollpos}, 0);
-      });
+        $('#content').removeClass('fixed').css('top', height)
+        $('body,html').animate({scrollTop: scrollpos}, 0)
+      })
     }
-    return false;
-  });
+    return false
+  })
 
   // delete/restore book button
   $('.delete-book').on('click', function(e) {
@@ -57,7 +57,7 @@ $(function() {
         $(o).hide()
         $itemDelete.show()
       }
-    });
+    })
   })
 
-});
+})
