511c3375800e31d52342fef092c11cdb9b5c7e3e
tab0902 <tabuchi@booxmix.com>
2019-11-17 19:21:52 +0900

    Add main.admin.PlaylistBookTabularInline

--- main/admin.py
+++ main/admin.py
@@ -34,7 +34,20 @@ class PlaylistInline(SlimTabularInline):
         return max_num
 
 
-class PlaylistBookInline(admin.StackedInline):
+class PlaylistBookTabularInline(admin.TabularInline):
+    model = Book.playlists.through
+    can_delete = True
+    show_change_link = False
+    exclude = ('description',)
+
+    def get_max_num(self, request, obj=None, **kwargs):
+        max_num = 0
+        if obj:
+            max_num = obj.playlists.count()
+        return max_num
+
+
+class PlaylistBookStackedInline(admin.StackedInline):
     model = Playlist.books.through
     can_delete = True
     show_change_link = False
@@ -42,7 +55,7 @@ class PlaylistBookInline(admin.StackedInline):
     def get_extra(self, request, obj=None, **kwargs):
         extra = 1
         if obj:
-            return extra - obj.books.count() if extra > obj.books.count() else 0
+            return extra - obj.playlistbook_set.count() if extra > obj.playlistbook_set.count() else 0
         return extra
 
 
@@ -51,6 +64,7 @@ class ThemeAdmin(admin.ModelAdmin):
     list_display = ('name', 'slug', 'sequence', 'created_at', )
     list_filter = ('created_at', 'updated_at', )
     search_fields = ('name', 'slug', 'description', )
+    inlines = (PlaylistInline,)
 
 
 @admin.register(Provider)
@@ -65,7 +79,7 @@ class BookAdmin(admin.ModelAdmin):
     list_display = ('__str__', 'isbn', 'created_at', )
     list_filter = ('created_at', 'updated_at', )
     search_fields = ('isbn', 'book_data_set__title', 'book_data_set__author', 'book_data_set__publisher', 'book_data_set__cover', 'book_data_set__affiliate_url', )
-    inlines = [BookDataInline]
+    inlines = (BookDataInline, PlaylistBookTabularInline,)
 
 
 @admin.register(Playlist)
@@ -73,4 +87,4 @@ class PlaylistAdmin(AllObjectsModelAdmin):
     list_display = ('title', 'user', 'theme', 'created_at', 'is_published', )
     list_filter = ('theme__name', 'is_published', 'created_at', 'updated_at', )
     search_fields = ('title', 'description', 'user__username', 'theme__name', 'books__isbn', 'books__book_data_set__title', 'books__book_data_set__author', 'books__book_data_set__publisher', )
-    inlines = [PlaylistBookInline]
+    inlines = (PlaylistBookStackedInline,)
