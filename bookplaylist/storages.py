5490b89cd1362627b023da7a385757b1b4d61d3d
tab0902 <tabuchi@booxmix.com>
2019-11-21 15:43:41 +0900

    Add /bookplaylist/storages.py

---
+++ bookplaylist/storages.py
@@ -0,0 +1,16 @@
+from storages.backends.gcloud import GoogleCloudStorage
+from storages.utils import setting
+
+
+class GoogleCloudMediaStorage(GoogleCloudStorage):
+
+    def __init__(self, *args, **kwargs):
+        kwargs['location'] = setting('GS_MEDIA_LOCATION', 'media')
+        super().__init__(*args, **kwargs)
+
+
+class GoogleCloudStaticStorage(GoogleCloudStorage):
+
+    def __init__(self, *args, **kwargs):
+        kwargs['location'] = setting('GS_STATIC_LOCATION', 'static')
+        super().__init__(*args, **kwargs)
